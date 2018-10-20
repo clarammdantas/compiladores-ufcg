15bf4a843928f581849bef7258570d023409ff1a
Arthur Costa <arthur.costa@ccc.ufcg.edu.br>
2018-10-20 15:41:04 -0300

    program.pas updated

--- src/main/pascal/program.pas
+++ src/main/pascal/program.pas
@@ -1,1 +1,34 @@
-isso é um teste
+program teste;
+
+procedure soma(x,y:integer);
+var
+z: integer;
+begin
+    z := x + y;
+end;
+
+function soma_func(x, y:integer):integer;
+var
+z: integer;
+begin
+    z := x + y;
+    soma_func := z;
+end;
+
+var 
+a: string;
+i, soma_res: integer;
+arr: array[1..10] of boolean;
+
+begin
+    a := 'Hello, world!';
+    i := 10;
+    
+    repeat
+        arr[i] := i > 3;
+        i := i - 1;
+    until i = 1;
+    
+    soma(1, 2);
+    soma_res := soma_func(1, 2);
+end.
