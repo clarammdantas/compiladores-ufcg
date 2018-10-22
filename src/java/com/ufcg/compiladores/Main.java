6ac1f23e7165e2885a3c73a6214387edef950d56
Victor Andrade de Almeida <victor.almeida@ccc.ufcg.edu.br>
2018-10-21 23:45:22 -0300

    Refatoração com Ant

--- src/java/com/ufcg/compiladores/Main.java
+++ src/java/com/ufcg/compiladores/Main.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 public class Main {
 	public static void main(String[] args) {
         try {
-            Parser p = new Parser(new Lexer(new FileReader("src/main/pascal/test.pas")));
+            Parser p = new Parser(new Lexer(new FileReader("src/pascal/test.pas")));
             Object result = p.parse().value;
 
             System.out.println("Compilation successful!");
